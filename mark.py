1e4465a136f5cc8b7093a09898db02d85cf46e50
Selvakumar G <65029376+gselva28@users.noreply.github.com>
2020-07-22 22:17:31 +0530

    Time to report the mark!!

---
+++ mark.py
@@ -0,0 +1,9 @@
+sub=int(input("enter mark:"))
+if sub in range(91,100):
+    print("grade A")
+elif sub in range(81,90):
+    print("grade B")
+elif sub in range(71,80):
+    print("grade C")
+else:
+   print("failed")
